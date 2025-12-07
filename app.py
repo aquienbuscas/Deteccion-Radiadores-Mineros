e5223d2e9ce296070dc1519ed72b1547ce4cbbd9
Remberto López <raulmamtilla2016@gmail.com>
2025-12-07 18:36:39 -0300

    Update HF_URL to point to bestyolo8m.pt

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import requests
 # ⚙️ CONFIGURACIÓN DEL MODELO
 # ==============================
 MODEL_PATH = "best.pt"
-HF_URL = "https://huggingface.co/Remberto/detector-radiadores/resolve/main/bestyolov11s.pt"
+HF_URL = "https://huggingface.co/Remberto/detector-radiadores/resolve/main/bestyolo8m.pt"
 
 # Descargar modelo si no existe localmente
 if not os.path.exists(MODEL_PATH):
